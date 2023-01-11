0f5f8fd46ea1446a6f6c7f6fbf8ebf7d0d4b2315
Patrick Hanna <contact@patrickhanna.dev>
2023-01-11 07:30:35 -0500

    exported page context from package

--- packages/react/src/index.ts
+++ packages/react/src/index.ts
@@ -4,6 +4,7 @@ export const router = Router
 export { default as createInertiaApp } from './createInertiaApp'
 export { default as Head } from './Head'
 export { default as Link } from './Link'
+export { default as PageContext } from './PageContext'
 export { default as useForm } from './useForm'
 export { default as usePage } from './usePage'
 export { default as useRemember } from './useRemember'
